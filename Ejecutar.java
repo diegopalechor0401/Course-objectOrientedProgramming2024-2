fae3bf3df28a72ed8b868ea3b0ef0ed8d7c18bac
diegopalechor0401 <167790348+diegopalechor0401@users.noreply.github.com>
2024-08-12 15:24:22 +0000

    taller #1 clase 1

--- Ejecutar.java
+++ Ejecutar.java
@@ -1,5 +1,27 @@
 public class Ejecutar {
         public static void main (String[]args){
             System.out.println("programacion Orientada a Objetos");
-        }
+
+            //comentario de una sola linea
+            
+            /*
+             * comentarios de 
+             * varias
+             * lineas
+             */
+            //JAVA la asignacion es con el = (igual)
+            //a = 5 -> a la variable a le asignamos un 5
+            //b = 10-> a la variable a le asignamos un 10
+            //nombre = "Cristiano" -->a la variable nombre le asignamos el texto "Cristiano"
+            //== (igual igual,doble igual)-->es de comparacion
+         //tener cuidado con las variables tiene un tipo de dato           
+         //int(enteros) es para la declaraion de varibles
+         //if es una variable (si entonces)
+         //long (reales)
+         //
+         int b = 2
+         int areaCuadrado = b * b;
+         System.out.println("area del cuadrado =" + areaCuadrado)
+
+         
 }
